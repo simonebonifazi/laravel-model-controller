06a4d8d37bab530c0e8404bf62ab09c8bc092d79
simonebonifazi <simonbonifraz@gmail.com>
2022-09-19 18:11:16 +0200

    added js and ready to start

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controller Demo</title>
 
+    @yield('cdns')
+
+    <!-- style -->
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <!-- js -->
+    <script defer src="{{ asset('js/app.js') }}"></script>
 </head>
 
 <body>
