136fe11f2428935d01b9cc648fed117827c6e3b0
simonebonifazi <simonbonifraz@gmail.com>
2022-09-20 08:39:13 +0200

    added titles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
+
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.main')
+
+@section('main-content')
+<ul>
+    @forelse($movies as $movie)
+    <li>
+        {{$movie->title}}
+    </li>
+    @empty
+    <h4> nessun libro da visualizzare... </h4>
+    @endforelse
+
+</ul>
+
+@endsection
